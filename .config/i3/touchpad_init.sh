beeac24d8e508f2e6afaab385ed8e1c888719b4d
dark-rabbit <darkrabbitlolagyar@gmail.com>
2016-08-18 19:10:38 +0200

    touchpad fix

--- .config/i3/touchpad_init.sh
+++ .config/i3/touchpad_init.sh
@@ -8,3 +8,6 @@ synclient VertTwoFingerScroll=1
 synclient VertEdgeScroll=1
 synclient VertScrollDelta=-100
 synclient MaxTapMove=3.4
+synclient TapButton1=1
+synclient TapButton2=2
+synclient TapButton3=3
